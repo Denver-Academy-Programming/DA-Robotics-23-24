b7e28cac259c249950bf95765c91c917451f49cf
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2023-04-20 18:46:31 -0600

    Moved Files into main directory area
    Moved Files into main directory area

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryCounter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TelemetryCounter.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.Locale;
+
 @TeleOp
-public class TelementryCounter extends LinearOpMode {
+public class TelemetryCounter extends LinearOpMode {
 
     @Override
     public void runOpMode() {
@@ -14,11 +16,11 @@ public class TelementryCounter extends LinearOpMode {
         // Wait until the user presses the run button
         waitForStart();
 
-        // Continously runs until the driver pressed the stop button
+        // Continuously runs until the driver pressed the stop button
         // Simply adds up a count variable which gets displayed in the telemetry window
         int count = 0;
         while (opModeIsActive()) {
-            String countString = String.format("Running: %d", count);
+            String countString = String.format(Locale.US, "Running: %d", count);
             telemetry.addData("Status", countString);
             telemetry.update();
             count += 1;
