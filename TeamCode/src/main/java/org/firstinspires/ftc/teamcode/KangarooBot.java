c4e7674316c8d68c823d94067e3ea107ede74437
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2023-05-25 15:19:36 -0600

    Fixed comments and reverse control issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KangarooBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KangarooBot.java
@@ -9,21 +9,22 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 public class KangarooBot extends LinearOpMode {
     @Override
     public void runOpMode() {
-        // Setup variables for bot
-        // Both motors the bot has attached to the Control Hub
+        // Setup the variables for bot.
+        // Both motors the bot has attached to the Control Hub.
         DcMotor motorLeft;
         DcMotor motorRight;
 
-        // Setup controller variables
+        // Setup variables for both motors.
         double motorLeftPower;
         double motorRightPower;
 
-        // Initialize and setup the modules connected to the Control Hub
-        // Also sets the left motor in reverse
+        // Initialize and setup the modules connected to the Control Hub.
+        // Also deliberately sets the right motor in reverse because its
+        // physically placed in reverse to the left motor.
         try {
             motorRight = hardwareMap.get(DcMotor.class, "motorRight");
             motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
-            motorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
         } catch (IllegalArgumentException e) {
             // Prettyfy error for non-tech peoples
             throw new RuntimeException(e);
@@ -32,21 +33,25 @@ public class KangarooBot extends LinearOpMode {
         telemetry.addData("Status", "Initialized\nWaiting to Start...");
         telemetry.update();
 
-        // Wait until the user presses the run button
+        // Wait until the user presses the run button.
         waitForStart();
 
-        // Checks for the input of gamepad1s left and right thumb sticks then
-        // applies that input to which motor it corresponds to
+        // Checks for the input of gamepad1s left and right thumb sticks
+        // then applies that input to which motor it corresponds to.
         while (opModeIsActive()) {
+            motorRightPower = gamepad1.right_stick_y;
+            motorLeftPower = gamepad1.left_stick_y;
 
-            motorRight.setPower(gamepad1.left_stick_y / 2);
-            motorLeft.setPower(gamepad1.right_stick_y / 2);
+            // Motor power is halved so its not so hard to control
+            motorRight.setPower(motorRightPower / 2);
+            motorLeft.setPower(motorLeftPower / 2);
 
             telemetry.addData("Right Motor Power", motorRight.getPower());
             telemetry.addData("Left Motor Power", motorLeft.getPower());
             telemetry.addData("Status", "Running");
             telemetry.update();
 
+            // Pressing B on the controller will stop the TeleOP script.
             if (gamepad1.b) {
                 requestOpModeStop();
             }
