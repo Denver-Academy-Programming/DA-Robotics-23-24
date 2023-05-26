c042a634772525bf8eced6d113b23a072bb1f1ea
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2023-05-26 17:38:35 -0600

    Power control and prettifying
    Started working on power level controls for the bot so speed is able to be set and manageable with the controller. Also finally got around to prettifying the initialization error for the KangarooBot script as well as comments and telemetry messages.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KangarooBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KangarooBot.java
@@ -18,22 +18,32 @@ public class KangarooBot extends LinearOpMode {
         double motorLeftPower;
         double motorRightPower;
 
+        // Setup variables for power control
+        // 1 - Fast
+        // 2 - Medium Speed
+        // 3 - Slow
+        int powerLevel;
+
         // Initialize and setup the modules connected to the Control Hub.
         // Also deliberately sets the right motor in reverse because its
-        // physically placed in reverse to the left motor.
+        // physically placed in a reverse position to the left motor.
         try {
             motorRight = hardwareMap.get(DcMotor.class, "motorRight");
             motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
             motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            powerLevel = 1;
         } catch (IllegalArgumentException e) {
-            // Prettyfy error for non-tech peoples
-            throw new RuntimeException(e);
+            throw new RuntimeException(
+                    "An error occurred with the initialization of the script!\n" +
+                    "Please check you have the right control scheme set on the\n" +
+                    "Driver Hub so it knows which modules are connected.\n" +
+                    "Full traceback:\n" + e
+            );
         }
 
-        telemetry.addData("Status", "Initialized\nWaiting to Start...");
+        // Display that the script/bot is ready and wait until the user presses the run button.
+        telemetry.addData("Status:", "Initialized!\nWaiting to Start...");
         telemetry.update();
-
-        // Wait until the user presses the run button.
         waitForStart();
 
         // Checks for the input of gamepad1s left and right thumb sticks
@@ -43,13 +53,23 @@ public class KangarooBot extends LinearOpMode {
             motorLeftPower = gamepad1.left_stick_y;
 
             // Motor power is halved so its not so hard to control
-            motorRight.setPower(motorRightPower / 2);
-            motorLeft.setPower(motorLeftPower / 2);
+            motorRight.setPower(motorRightPower / powerLevel);
+            motorLeft.setPower(motorLeftPower / powerLevel);
 
-            telemetry.addData("Right Motor Power", motorRight.getPower());
-            telemetry.addData("Left Motor Power", motorLeft.getPower());
-            telemetry.addData("Status", "Running");
+            // Display/Update information to the telemetry screen of the Driver Station
+            telemetry.addData("Power Level:", powerLevel);
+            telemetry.addData("Right Motor Power:", motorRight.getPower());
+            telemetry.addData("Left Motor Power:", motorLeft.getPower());
+            telemetry.addData("Status:", "Running!");
+            telemetry.addLine("Press B button to stop script...");
             telemetry.update();
+
+            // Power control for the bot, makes sure that powerLevel says within a 1-3 range
+            if (gamepad1.dpad_up & (powerLevel < 3)) {
+                powerLevel -= 1;
+            } else if (gamepad1.dpad_down & (powerLevel > 1)) {
+                powerLevel += 1;
+            }
 
             // Pressing B on the controller will stop the TeleOP script.
             if (gamepad1.b) {
